dd8f021d1639f1d5f013efa9bd5988e3cee17115
jluch <darkdroguza@hotmail.fr>
2023-03-26 17:37:12 +0200

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mon-api.com'
+  apiUrl: 'https://pokeapi.co/api/v2'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: '${process.env.API_URL}'
+  apiUrl: 'https://pokeapi.co/api/v2'
 };
