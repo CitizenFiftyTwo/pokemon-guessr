5633e7c58825da6836df13acae0a1964b98f4f7e
jluch <darkdroguza@hotmail.fr>
2023-03-26 01:15:28 +0100

    init should dispatch LoadPokemonNameAction

--- src/app/modules/guessr/guessr-wrapper/guessr-wrapper.component.spec.ts
+++ src/app/modules/guessr/guessr-wrapper/guessr-wrapper.component.spec.ts
@@ -2,24 +2,44 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GuessrWrapperComponent } from './guessr-wrapper.component';
 import { TranslateModule } from "@ngx-translate/core";
+import { MockStore, provideMockStore } from "@ngrx/store/testing";
+import { selectNumberOfPokemons } from "../../../stores/pokemon";
+import { LoadPokemonNameAction } from "../../../stores/pokemon/pokemon.action";
 
 describe('GuessrWrapperComponent', () => {
   let component: GuessrWrapperComponent;
   let fixture: ComponentFixture<GuessrWrapperComponent>;
+  let mockStore: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [GuessrWrapperComponent],
-      imports: [TranslateModule.forRoot()]
-    })
-      .compileComponents();
+      imports: [TranslateModule.forRoot()],
+      providers: [
+        provideMockStore({
+          selectors: [
+            {
+              selector: selectNumberOfPokemons,
+              value: 151
+            }
+          ]
+        }),
+      ]
+    }).compileComponents();
 
+    mockStore = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(GuessrWrapperComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  describe('OnInit', () => {
+    it('should dispatch LoadPokemonNameAction', () => {
+      let dispatchSpyOn = spyOn(mockStore, 'dispatch');
+
+      component.ngOnInit();
+
+      expect(dispatchSpyOn).toHaveBeenCalledWith(LoadPokemonNameAction({pokemonIndex: 150}));
+    });
+  })
 });
--- src/app/modules/guessr/guessr-wrapper/guessr-wrapper.component.ts
+++ src/app/modules/guessr/guessr-wrapper/guessr-wrapper.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from "@ngrx/store";
+import { filter, map, Observable, of, take } from "rxjs";
+import { LoadPokemonNameAction } from "../../../stores/pokemon/pokemon.action";
+import { selectNumberOfPokemons } from "../../../stores/pokemon";
 
 @Component({
   selector: 'app-guessr-wrapper',
   templateUrl: './guessr-wrapper.component.html',
   styleUrls: ['./guessr-wrapper.component.scss']
 })
-export class GuessrWrapperComponent {
+export class GuessrWrapperComponent implements OnInit {
+
+  numberOfPokemons$: Observable<number> = of(0)
+
+  constructor(private store: Store) {
+  }
+
+  ngOnInit(): void {
+    this.numberOfPokemons$ = this.store.select(selectNumberOfPokemons)
+    this.numberOfPokemons$.pipe(
+      filter(numberOfPokemon => numberOfPokemon > 0),
+      take(1),
+      map(numberOfPokemon => LoadPokemonNameAction({pokemonIndex: numberOfPokemon - 1}))
+    ).subscribe(action => this.store.dispatch(action));
+  }
 
 }
