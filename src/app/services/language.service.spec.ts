f6060e8e62d88a652635b4a3ef740e8250127670
jluch <darkdroguza@hotmail.fr>
2023-03-30 18:12:28 +0200

    lang service

---
+++ src/app/services/language.service.spec.ts
@@ -0,0 +1,49 @@
+import { TestBed } from '@angular/core/testing';
+
+import { LanguageService } from './language.service';
+import { TranslateService } from "@ngx-translate/core";
+
+describe('LanguageService', () => {
+  let service: LanguageService;
+  let translateServiceSpy: jasmine.SpyObj<TranslateService>;
+
+  beforeEach(() => {
+    translateServiceSpy = jasmine.createSpyObj('TranslateService', ['getBrowserLang']);
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: TranslateService,
+          useValue: translateServiceSpy
+        },
+      ]
+    });
+    service = TestBed.inject(LanguageService);
+  });
+
+
+  describe('getDefaultLanguage', () => {
+    it('should be browser language for en', () => {
+      translateServiceSpy.getBrowserLang.and.returnValue('en');
+
+      const browserLanguage = service.getLanguage();
+
+      expect(browserLanguage).toEqual('en');
+    });
+
+    it('should be browser language for fr', () => {
+      translateServiceSpy.getBrowserLang.and.returnValue('fr');
+
+      const browserLanguage = service.getLanguage();
+
+      expect(browserLanguage).toEqual('fr');
+    });
+
+    it('should be fr when browser language is not known', () => {
+      translateServiceSpy.getBrowserLang.and.returnValue('de');
+
+      const browserLanguage = service.getLanguage();
+
+      expect(browserLanguage).toEqual('fr');
+    });
+  })
+});
---
+++ src/app/services/language.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { TranslateService } from "@ngx-translate/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LanguageService {
+
+  private readonly KNOWN_LANGUAGES = ['fr', 'en']
+
+  constructor(private translateService: TranslateService) {
+  }
+
+  getLanguage(): string {
+    const browserLanguage = this.translateService.getBrowserLang();
+    return this.KNOWN_LANGUAGES.includes(browserLanguage || '') ? browserLanguage!! : this.KNOWN_LANGUAGES[0]
+  }
+}
